8de57c61cb1c1beaa15c6e2a8924fc7a11b75932
Rayhan Janam <rayhanjanam@gmail.com>
2018-06-28 11:03:07 +0600

    Initial Commit

---
+++ RNShell/RNShell.h
@@ -0,0 +1,19 @@
+//
+//  RNShell.h
+//  RNShell
+//
+//  Created by Rayhan Janam on 6/28/18.
+//  Copyright © 2018 Rayhan Janam. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for RNShell.
+FOUNDATION_EXPORT double RNShellVersionNumber;
+
+//! Project version string for RNShell.
+FOUNDATION_EXPORT const unsigned char RNShellVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RNShell/PublicHeader.h>
+
+
